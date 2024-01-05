0e32b5526f7d6c9070b574764a9356d7ae33ed3a
Aiden Knight <worgenaiden@gmail.com>
2024-01-05 16:51:20 +0000

    Added evade steering behaviour

--- Assets/Scripts/SteeringBehaviours/SteeringBehaviour_Evade.cs
+++ Assets/Scripts/SteeringBehaviours/SteeringBehaviour_Evade.cs
@@ -17,8 +17,26 @@ public class SteeringBehaviour_Evade : SteeringBehaviour
 
     public override Vector2 CalculateForce()
     {
-        //delete me
-        return Vector2.zero;
+        Vector2 betweenVector = m_EvadingEntity.transform.position - m_Manager.m_Entity.transform.position;
+        if (Maths.Magnitude(betweenVector) > m_EvadeRadius) return Vector2.zero;
+
+        float opponentSpeed = Maths.Magnitude(m_EvadingEntity.m_Velocity);
+        Vector2 fleePos;
+
+        if(opponentSpeed > 0)
+        {
+            float predictionTime = Maths.Magnitude(betweenVector) / (m_Manager.m_Entity.m_MaxSpeed + opponentSpeed);
+            fleePos = (Vector2)m_EvadingEntity.transform.position + m_EvadingEntity.m_Velocity * predictionTime;
+        }
+        else
+        {
+            fleePos = m_EvadingEntity.transform.position;
+        }
+
+        Vector2 negativeDir = (Vector2)m_Manager.m_Entity.transform.position - fleePos;
+        m_DesiredVelocity = negativeDir * m_Manager.m_Entity.m_MaxSpeed;
+
+        return m_Weight * (m_DesiredVelocity - m_Manager.m_Entity.m_Velocity);
     }
 
     protected override void OnDrawGizmosSelected()
